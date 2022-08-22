1c1229b3513de42119c0c49f0527d81bf80c1176
Electromorphous <pstation.474@gmail.com>
2022-08-23 01:16:13 +0530

    fix alert z index bug

--- src/components/CustomAlert.jsx
+++ src/components/CustomAlert.jsx
@@ -1,13 +1,28 @@
 import { useEffect } from "react";
 import { IconButton, Collapse, Alert } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
+import { makeStyles } from "@mui/styles";
 
 // open: boolean
 // setOpen: () => void
 // severity: string
 // message: string
 
+const useStyles = makeStyles((theme) => ({
+  alertContainer: {
+    position: "fixed",
+    top: 61,
+    left: "50%",
+    transform: "translate(-50%, 0)",
+    width: "90%",
+    maxWidth: 500,
+    zIndex: theme.zIndex.drawer + 1,
+  },
+}));
+
 function CustomAlert({ open, setOpen, severity, message }) {
+  const classes = useStyles();
+
   useEffect(() => {
     let timer = setTimeout(() => {
       if (open) setOpen(false);
@@ -18,17 +33,7 @@ function CustomAlert({ open, setOpen, severity, message }) {
   }, [open]);
 
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: 30,
-        left: "50%",
-        transform: "translate(-50%, 0)",
-        width: "90%",
-        maxWidth: 500,
-        zIndex: 99,
-      }}
-    >
+    <div className={classes.alertContainer}>
       <Collapse in={open} timeout={200}>
         <Alert
           severity={severity}
