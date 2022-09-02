71302135b00a4547f1df29803b52ae47d59e7979
VIVEK PANDEY <vivekk.18.amca@acharya.ac.in>
2022-09-02 10:40:17 +0530

    School,Organization,Employee Type

--- src/App.jsx
+++ src/App.jsx
@@ -14,78 +14,16 @@ import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import FormExample from "./containers/examples/FormExample";
 
-import BoardCreation from "./containers/Board/BoardCreation";
-import BoardIndex from "./containers/Board/BoardIndex";
-import MenuCreation from "./containers/Menu/MenuCreation";
-import MenuIndex from "./containers/Menu/MenuIndex";
-import MenuUpdate from "./containers/Menu/MenuUpdate";
-
-import AcademicYearCreation from "./containers/AcademicYear/AcademicYearCreation";
-import AcademicYearIndex from "./containers/AcademicYear/AcademicYearIndex";
-import AcademicYearUpdate from "./containers/AcademicYear/AcademicYearUpdate";
 import SchoolCreation from "./containers/School/SchoolCreation";
 import SchoolIndex from "./containers/School/SchoolIndex";
 import SchoolUpdate from "./containers/School/SchoolUpdate";
 import OrganizationCreation from "./containers/Organization/OrganizationCreation";
 import OrganizationIndex from "./containers/Organization/OrganizationIndex";
 import OrganizationUpdate from "./containers/Organization/OrganizationUpdate";
-import JobtypeCreation from "./containers/JobType/JobtypeCreation";
-import JobtypeIndex from "./containers/JobType/JobtypeIndex";
-import JobtypeUpdate from "./containers/JobType/JobtypeUpdate";
+
 import EmptypeCreation from "./containers/EmployeeType/EmptypeCreation";
 import EmptypeIndex from "./containers/EmployeeType/EmptypeIndex";
 import EmptypeUpdate from "./containers/EmployeeType/EmptypeUpdate";
-
-import AdmCategoryCreation from "./containers/AdmissionCategory/AdmCategoryCreation";
-import AdmCategoryIndex from "./containers/AdmissionCategory/AdmCategoryIndex";
-import AdmCategoryUpdate from "./containers/AdmissionCategory/AdmCategoryUpdate";
-import AdmSubCategoryCreation from "./containers/AdmissionSubCategory/AdmSubCategoryCreation";
-import AdmSubCategoryIndex from "./containers/AdmissionSubCategory/AdmSubCategoryIndex";
-import AdmSubCategoryUpdate from "./containers/AdmissionSubCategory/AdmSubCategoryUpdate";
-import BoardUpdate from "./containers/Board/BoardUpdate";
-import ProgramCreation from "./containers/Program/ProgramCreation";
-import ProgramIndex from "./containers/Program/ProgramIndex";
-import ProgramUpdate from "./containers/Program/ProgramUpdate";
-import ProgramtypeCreation from "./containers/ProgramType/ProgramtypeCreation";
-import ProgramtypeIndex from "./containers/ProgramType/ProgramtypeIndex";
-import ProgramtypeUpdate from "./containers/ProgramType/ProgramtypeUpdate";
-import CurrencytypeCreation from "./containers/CurrencyType/CurrencytypeCreation";
-import CurrencytypeIndex from "./containers/CurrencyType/CurrencytypeIndex";
-import CurrencytypeUpdate from "./containers/CurrencyType/CurrencytypeUpdate";
-import GraduationCreation from "./containers/Graduation/GraduationCreation";
-import GraduationIndex from "./containers/Graduation/GraduationIndex";
-import GraduationUpdate from "./containers/Graduation/GraduationUpdate";
-import ModuleCreation from "./containers/Module/ModuleCreation";
-import ModuleIndex from "./containers/Module/ModuleIndex";
-import ModuleUpdate from "./containers/Module/ModuleUpdate";
-import SubmenuCreation from "./containers/SubMenu/SubmenuCreation";
-import SubmenuIndex from "./containers/SubMenu/SubmenuIndex";
-import SubmenuUpdate from "./containers/SubMenu/SubmenuUpdate";
-import GroupCreation from "./containers/Group/GroupCreation";
-import GroupIndex from "./containers/Group/GroupIndex";
-import GroupUpdate from "./containers/Group/GroupUpdate";
-import LedgerCreation from "./containers/Ledger/LedgerCreation";
-import LedgerIndex from "./containers/Ledger/LedgerIndex";
-import LedgerUpdate from "./containers/Ledger/LedgerUpdate";
-import TallyheadCreation from "./containers/TallyHead/TallyheadCreation";
-import TallyheadIndex from "./containers/TallyHead/TallyheadIndex";
-import TallyheadUpdate from "./containers/TallyHead/TallyheadUpdate";
-import FinancialyearCreation from "./containers/FinancialYear/FinancialyearCreation";
-import FinancialyearIndex from "./containers/FinancialYear/FinancialyearIndex";
-import FinancialyearUpdate from "./containers/FinancialYear/FinancialyearUpdate";
-import ProgramSpecializationCreation from "./containers/ProgramSpecialization/ProgramSpecializationCreation";
-import ProgramSpecializationIndex from "./containers/ProgramSpecialization/ProgramSpecializationIndex";
-import ProgramSpecializationUpdate from "./containers/ProgramSpecialization/ProgramSpecializationUpdate";
-import ProgramAssCreation from "./containers/ProgramAssignment/ProgramAssCreation";
-import InstituteMaster from "./containers/Tab/InstituteMaster";
-import ProgramAssIndex from "./containers/ProgramAssignment/ProgramAssIndex";
-import ProgramAssUpdate from "./containers/ProgramAssignment/ProgramAssUpdate";
-import DepartmentCreation from "./containers/Department/DepartmentCreation";
-import DepartmentIndex from "./containers/Department/DepartmentIndex";
-import DepartmentUpdate from "./containers/Department/DepartmentUpdate";
-import DepartmentAssignmentCreation from "./containers/DepartmentAssignment/DepartmentAssignmentCreation";
-import DepartmentAssignmentIndex from "./containers/DepartmentAssignment/DepartmentAssignmentIndex";
-import DepartmentAssignmentUpdate from "./containers/DepartmentAssignment/DepartmentAssignmentUpdate";
 
 function App() {
   return (
@@ -122,6 +60,11 @@ function App() {
                 exact
                 path="/EmptypeUpdate/:id"
                 element={<EmptypeUpdate />}
+              />
+              <Route
+                exact
+                path="/OrganizationCreation"
+                element={<OrganizationCreation />}
               />
 
               <Route
