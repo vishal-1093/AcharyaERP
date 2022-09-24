11a2ea02d96550ba7fc030b6fe71112edffcd98d
Electromorphous <pstation.474@gmail.com>
2022-09-25 01:38:35 +0530

    create menu icons utility file

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,51 +12,9 @@ import {
 import MuiDrawer from "@mui/material/Drawer";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import ExpandLess from "@mui/icons-material/ExpandLess";
+import iconsList from "../utils/MenuIcons";
 import { makeStyles } from "@mui/styles";
 import { useNavigate } from "react-router-dom";
-
-// Menu icons
-import HomeIcon from "@mui/icons-material/Home";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import StarRoundedIcon from "@mui/icons-material/StarRounded";
-import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
-import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
-import AcUnitRoundedIcon from "@mui/icons-material/AcUnitRounded";
-import AccessibilityNewRoundedIcon from "@mui/icons-material/AccessibilityNewRounded";
-import AirportShuttleRoundedIcon from "@mui/icons-material/AirportShuttleRounded";
-import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
-import BlurOnIcon from "@mui/icons-material/BlurOn";
-
-const iconsList = [
-  { name: "Home", icon: <HomeIcon fontSize="large" /> },
-  { name: "Favorite", icon: <FavoriteIcon fontSize="large" /> },
-  { name: "Star", icon: <StarRoundedIcon fontSize="large" /> },
-  {
-    name: "Time",
-    icon: <AccessTimeFilledRoundedIcon fontSize="large" />,
-  },
-  {
-    name: "User",
-    icon: <AccountCircleRoundedIcon fontSize="large" />,
-  },
-  { name: "AC", icon: <AcUnitRoundedIcon fontSize="large" /> },
-  {
-    name: "Access",
-    icon: <AccessibilityNewRoundedIcon fontSize="large" />,
-  },
-  {
-    name: "Truck",
-    icon: <AirportShuttleRoundedIcon fontSize="large" />,
-  },
-  {
-    name: "Stars",
-    icon: <AutoAwesomeRoundedIcon fontSize="large" />,
-  },
-  {
-    name: "Default",
-    icon: <BlurOnIcon fontSize="large" />,
-  },
-];
 
 const useStyles = makeStyles((theme) => ({
   listItemButton: {
---
+++ src/utils/MenuIcons.js
@@ -0,0 +1,43 @@
+import HomeIcon from "@mui/icons-material/Home";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import StarRoundedIcon from "@mui/icons-material/StarRounded";
+import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import AcUnitRoundedIcon from "@mui/icons-material/AcUnitRounded";
+import AccessibilityNewRoundedIcon from "@mui/icons-material/AccessibilityNewRounded";
+import AirportShuttleRoundedIcon from "@mui/icons-material/AirportShuttleRounded";
+import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
+import BlurOnIcon from "@mui/icons-material/BlurOn";
+
+const iconsList = [
+  { name: "Home", icon: <HomeIcon fontSize="large" /> },
+  { name: "Favorite", icon: <FavoriteIcon fontSize="large" /> },
+  { name: "Star", icon: <StarRoundedIcon fontSize="large" /> },
+  {
+    name: "Time",
+    icon: <AccessTimeFilledRoundedIcon fontSize="large" />,
+  },
+  {
+    name: "User",
+    icon: <AccountCircleRoundedIcon fontSize="large" />,
+  },
+  { name: "AC", icon: <AcUnitRoundedIcon fontSize="large" /> },
+  {
+    name: "Access",
+    icon: <AccessibilityNewRoundedIcon fontSize="large" />,
+  },
+  {
+    name: "Truck",
+    icon: <AirportShuttleRoundedIcon fontSize="large" />,
+  },
+  {
+    name: "Stars",
+    icon: <AutoAwesomeRoundedIcon fontSize="large" />,
+  },
+  {
+    name: "Default",
+    icon: <BlurOnIcon fontSize="large" />,
+  },
+];
+
+export default iconsList;
