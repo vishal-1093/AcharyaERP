47076b79f580ffd9256d3b0bf2b233b3ebbf2c6b
Electromorphous <pstation.474@gmail.com>
2022-11-22 01:10:46 +0530

    verify files

--- src/pages/forms/accountMaster/VoucherAssignmentForm.jsx
+++ src/pages/forms/accountMaster/VoucherAssignmentForm.jsx
@@ -167,9 +167,7 @@ function VoucherAssignmentForm() {
             }))
           );
         })
-        .catch((err) => {
-          console.error(err);
-        });
+        .catch((err) => console.error(err));
     }
   };
 
@@ -297,152 +295,152 @@ function VoucherAssignmentForm() {
         });
     }
   };
+
   return (
-    <>
-      <Box component="form" overflow="hidden" p={1}>
-        <CustomModal
-          open={modalOpen}
-          setOpen={setModalOpen}
-          title={modalContent.title}
-          message={modalContent.message}
-          buttons={modalContent.buttons}
-        />
-        <FormWrapper>
-          <Grid
-            container
-            alignItems="center"
-            rowSpacing={4}
-            columnSpacing={{ xs: 2, md: 4 }}
-          >
+    <Box component="form" overflow="hidden" p={1}>
+      <CustomModal
+        open={modalOpen}
+        setOpen={setModalOpen}
+        title={modalContent.title}
+        message={modalContent.message}
+        buttons={modalContent.buttons}
+      />
+      <FormWrapper>
+        <Grid
+          container
+          alignItems="center"
+          rowSpacing={4}
+          columnSpacing={{ xs: 2, md: 4 }}
+        >
+          <Grid item xs={12} md={4}>
+            <CustomAutocomplete
+              label="Voucher Head"
+              name="voucherId"
+              value={values.voucherId}
+              options={voucherOptions}
+              disabled={!isNew}
+              handleChangeAdvance={handleChangeAdvance}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <CustomAutocomplete
+              label="Ledger"
+              name="ledgerId"
+              value={values.ledgerId}
+              options={legderOptions}
+              handleChangeAdvance={handleChangeAdvance}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <CustomSelect
+              label="Voucher Type"
+              name="voucherType"
+              value={values.voucherType}
+              items={[
+                { value: "inflow", label: "Inflow" },
+                { value: "outflow", label: "OutFlow" },
+                { value: "journal", label: "Journal" },
+                { value: "all", label: "All" },
+              ]}
+              handleChange={handleChange}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <CustomRadioButtons
+              name="budgetHead"
+              label="Budget Head"
+              value={values.budgetHead}
+              items={[
+                { label: "Yes", value: true },
+                { label: "No", value: false },
+              ]}
+              handleChange={handleChange}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <CustomRadioButtons
+              name="cashBank"
+              label="Cash / Bank"
+              value={values.cashBank}
+              items={[
+                { label: "Yes", value: true },
+                { label: "No", value: false },
+              ]}
+              handleChange={handleChange}
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12} md={4}>
+            <CustomTextField
+              name="voucherPriority"
+              label="Priority"
+              value={values.voucherPriority}
+              handleChange={handleChange}
+              errors={errorMessages.voucherPriority}
+              checks={checks.voucherPriority}
+              required
+            />
+          </Grid>
+          {isNew ? (
             <Grid item xs={12} md={4}>
-              <CustomAutocomplete
-                label="Voucher Head"
-                name="voucherId"
-                value={values.voucherId}
-                options={voucherOptions}
-                disabled={!isNew}
+              <CustomMultipleAutocomplete
+                name="school"
+                label="School"
+                value={values.school}
+                options={schoolOptions}
                 handleChangeAdvance={handleChangeAdvance}
                 required
               />
             </Grid>
-            <Grid item xs={12} md={4}>
-              <CustomAutocomplete
-                label="Ledger"
-                name="ledgerId"
-                value={values.ledgerId}
-                options={legderOptions}
-                handleChangeAdvance={handleChangeAdvance}
-                required
-              />
-            </Grid>
+          ) : (
             <Grid item xs={12} md={4}>
               <CustomSelect
-                label="Voucher Type"
-                name="voucherType"
-                value={values.voucherType}
-                items={[
-                  { value: "inflow", label: "Inflow" },
-                  { value: "outflow", label: "OutFlow" },
-                  { value: "journal", label: "Journal" },
-                  { value: "all", label: "All" },
-                ]}
-                handleChange={handleChange}
-                required
+                name="school"
+                label="School"
+                value={values.school}
+                items={schoolOptions}
+                disabled
               />
             </Grid>
-            <Grid item xs={12} md={2}>
-              <CustomRadioButtons
-                name="budgetHead"
-                label="Budget Head"
-                value={values.budgetHead}
-                items={[
-                  { label: "Yes", value: true },
-                  { label: "No", value: false },
-                ]}
-                handleChange={handleChange}
-                required
-              />
-            </Grid>
-            <Grid item xs={12} md={2}>
-              <CustomRadioButtons
-                name="cashBank"
-                label="Cash / Bank"
-                value={values.cashBank}
-                items={[
-                  { label: "Yes", value: true },
-                  { label: "No", value: false },
-                ]}
-                handleChange={handleChange}
-                required
-              />
-            </Grid>
+          )}
 
-            <Grid item xs={12} md={4}>
-              <CustomTextField
-                name="voucherPriority"
-                label="Priority"
-                value={values.voucherPriority}
-                handleChange={handleChange}
-                errors={errorMessages.voucherPriority}
-                checks={checks.voucherPriority}
-                required
-              />
-            </Grid>
-            {isNew ? (
-              <Grid item xs={12} md={4}>
-                <CustomMultipleAutocomplete
-                  name="school"
-                  label="School"
-                  value={values.school}
-                  options={schoolOptions}
-                  handleChangeAdvance={handleChangeAdvance}
-                  required
-                />
-              </Grid>
-            ) : (
-              <Grid item xs={12} md={4}>
-                <CustomSelect
-                  name="school"
-                  label="School"
-                  value={values.school}
-                  items={schoolOptions}
-                  disabled
-                />
-              </Grid>
-            )}
-
-            <Grid item xs={12}>
-              <Grid
-                container
-                alignItems="center"
-                justifyContent="flex-end"
-                textAlign="right"
-              >
-                <Grid item xs={6} sm={4} md={2}>
-                  <Button
-                    style={{ borderRadius: 7 }}
-                    variant="contained"
-                    color="primary"
-                    disabled={loading}
-                    onClick={isNew ? handleCreate : handleUpdate}
-                  >
-                    {loading ? (
-                      <CircularProgress
-                        size={25}
-                        color="blue"
-                        style={{ margin: "2px 13px" }}
-                      />
-                    ) : (
-                      <strong>{isNew ? "Create" : "Update"}</strong>
-                    )}
-                  </Button>
-                </Grid>
+          <Grid item xs={12}>
+            <Grid
+              container
+              alignItems="center"
+              justifyContent="flex-end"
+              textAlign="right"
+            >
+              <Grid item xs={6} sm={4} md={2}>
+                <Button
+                  style={{ borderRadius: 7 }}
+                  variant="contained"
+                  color="primary"
+                  disabled={loading}
+                  onClick={isNew ? handleCreate : handleUpdate}
+                >
+                  {loading ? (
+                    <CircularProgress
+                      size={25}
+                      color="blue"
+                      style={{ margin: "2px 13px" }}
+                    />
+                  ) : (
+                    <strong>{isNew ? "Create" : "Update"}</strong>
+                  )}
+                </Button>
               </Grid>
             </Grid>
           </Grid>
-        </FormWrapper>
-      </Box>
-    </>
+        </Grid>
+      </FormWrapper>
+    </Box>
   );
 }
+
 export default VoucherAssignmentForm;
