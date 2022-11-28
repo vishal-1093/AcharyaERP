8df04ecac432f750d0bbf09d3210dfbe22fb52ec
Hanumantha B <hanumanthbs27@gmail.com>
2022-11-28 12:32:13 +0530

    Update Menu Icons

--- src/utils/MenuIcons.js
+++ src/utils/MenuIcons.js
@@ -9,6 +9,29 @@ import AirportShuttleRoundedIcon from "@mui/icons-material/AirportShuttleRounded
 import AutoAwesomeRoundedIcon from "@mui/icons-material/AutoAwesomeRounded";
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 import BlurOnIcon from "@mui/icons-material/BlurOn";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import RememberMeIcon from "@mui/icons-material/RememberMe";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import DisplaySettingsIcon from "@mui/icons-material/DisplaySettings";
+import RoomServiceIcon from "@mui/icons-material/RoomService";
+import ContactSupportIcon from "@mui/icons-material/ContactSupport";
+import PrivacyTipIcon from "@mui/icons-material/PrivacyTip";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import FlagIcon from "@mui/icons-material/Flag";
+import ReportIcon from "@mui/icons-material/Report";
+import BugReportIcon from "@mui/icons-material/BugReport";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import DiamondIcon from "@mui/icons-material/Diamond";
+import SchoolIcon from "@mui/icons-material/School";
+import GradeIcon from "@mui/icons-material/Grade";
+import GiteIcon from "@mui/icons-material/Gite";
+import DesktopWindowsIcon from "@mui/icons-material/DesktopWindows";
 
 const iconsList = [
   { name: "Home", icon: <HomeIcon sx={{ fontSize: 23 }} /> },
@@ -38,6 +61,98 @@ const iconsList = [
   {
     name: "Rupee",
     icon: <CurrencyRupeeIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Account Tree",
+    icon: <AccountTreeIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Inventory",
+    icon: <InventoryIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Account Balance",
+    icon: <AccountBalanceIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Add A Photo",
+    icon: <AddAPhotoIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Admin Panel Settings",
+    icon: <AdminPanelSettingsIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Account Box",
+    icon: <AccountBoxIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Assignment Ind",
+    icon: <AssignmentIndIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Remember Me",
+    icon: <RememberMeIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Settings Applications",
+    icon: <SettingsApplicationsIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Display Settings",
+    icon: <DisplaySettingsIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Room Service",
+    icon: <RoomServiceIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Contact Support",
+    icon: <ContactSupportIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Privacy Tip",
+    icon: <PrivacyTipIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Assessment",
+    icon: <AssessmentIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Flag",
+    icon: <FlagIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Report",
+    icon: <ReportIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Bug Report",
+    icon: <BugReportIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Psychology",
+    icon: <PsychologyIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Diamond",
+    icon: <DiamondIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "School",
+    icon: <SchoolIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Grade",
+    icon: <GradeIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "Gite",
+    icon: <GiteIcon sx={{ fontSize: 23 }} />,
+  },
+  {
+    name: "DesktopWindows",
+    icon: <DesktopWindowsIcon sx={{ fontSize: 23 }} />,
   },
   {
     name: "Default",
