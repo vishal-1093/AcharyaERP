4f4adf53e346ed513549e7e76bf41802521c07f8
Hanumantha B <hanumanthbs27@gmail.com>
2022-12-21 16:06:01 +0530

    changes in voucher head assignment

--- src/containers/indeces/accountMaster/VoucherAssignmentIndex.jsx
+++ src/containers/indeces/accountMaster/VoucherAssignmentIndex.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import AddIcon from "@mui/icons-material/Add";
 import EditIcon from "@mui/icons-material/Edit";
 import { Check, HighlightOff } from "@mui/icons-material";
-import { Grid, Box, IconButton, Button } from "@mui/material";
+import { Grid, Box, IconButton, Button, Typography } from "@mui/material";
 import CustomModal from "../../../components/CustomModal";
 import ModalWrapper from "../../../components/ModalWrapper";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
@@ -84,11 +84,12 @@ function VoucherAssignmentIndex() {
         });
   };
 
-  const handleOpeningBalance = (params) => {
+  const handleOpeningBalance = async (params) => {
     setOpenWrapper(true);
-    axios
+    await axios
       .get(`/api/finance/VoucherHead/${params.row.id}`)
       .then((res) => {
+        console.log(res.data.data);
         setData(res.data.data);
       })
       .catch((error) => {
@@ -214,7 +215,7 @@ function VoucherAssignmentIndex() {
     <>
       <ModalWrapper
         open={openWrapper}
-        title="Update OB"
+        title=""
         maxWidth={500}
         setOpen={setOpenWrapper}
       >
@@ -226,6 +227,9 @@ function VoucherAssignmentIndex() {
             rowSpacing={4}
             columnSpacing={{ xs: 2, md: 4 }}
           >
+            <Grid item xs={12}>
+              <Typography variant="subtitle2">Update OB</Typography>
+            </Grid>
             <Grid item xs={12}>
               <CustomTextField
                 label="Amount"
