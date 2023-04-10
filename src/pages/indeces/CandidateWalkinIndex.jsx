e55232375ad7790421f9700d0453f645f9dff11d
Hanumantha B <hanumanthbs27@gmail.com>
2023-04-10 18:45:24 +0530

    employee index page size

--- src/pages/indeces/CandidateWalkinIndex.jsx
+++ src/pages/indeces/CandidateWalkinIndex.jsx
@@ -221,7 +221,7 @@ function CandidateWalkinIndex() {
   const getData = async () => {
     await axios
       .get(
-        `/api/student/EditCandidateDetails?page=${0}&page_size=${100}&sort=created_date`
+        `/api/student/EditCandidateDetails?page=${0}&page_size=${10000}&sort=created_date`
       )
       .then((res) => {
         setRows(res.data.data.Paginated_data.content);
