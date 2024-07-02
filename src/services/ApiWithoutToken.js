e0a73fe001540b62ec5e3b6003aaa0d3cb38e937
hanumanthbs <74710737+hanumanthbs@users.noreply.github.com>
2024-07-02 12:00:20 +0530

    changes

--- src/services/ApiWithoutToken.js
+++ src/services/ApiWithoutToken.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 // Local
-const apiUrl = `http://192.168.0.104:8080`;
+const apiUrl = `https://www.stageapi-acharyainstitutes.in/Acharya_University`;
 
 // Staging backend
 // const apiUrl = `https://www.stageapi-acharyainstitutes.in`;
