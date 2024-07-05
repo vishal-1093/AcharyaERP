d948bd802b6ebb7247f6808e21850ec0e38990e7
hanumanthbs <74710737+hanumanthbs@users.noreply.github.com>
2024-07-05 10:30:01 +0530

    employee and student changes

--- src/services/ApiWithoutToken.js
+++ src/services/ApiWithoutToken.js
@@ -1,12 +1,12 @@
 import Axios from "axios";
 
 // Local
-const apiUrl = `http://192.168.0.104:8080`;
+// const apiUrl = `http://192.168.0.104:8080`;
 
 // Staging backend
 // const apiUrl = `https://www.stageapi-acharyainstitutes.in`;
 // const apiUrl = `http://ec2-15-206-202-184.ap-south-1.compute.amazonaws.com:8081/Acharya_University`;
-// const apiUrl = `https://www.stageapi-acharyainstitutes.in/Acharya_University`;
+const apiUrl = `https://www.stageapi-acharyainstitutes.in/Acharya_University`;
 
 const axios = Axios.create({
   baseURL: apiUrl,
