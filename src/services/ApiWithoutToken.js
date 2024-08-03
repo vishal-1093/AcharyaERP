1dcee144b2ea3c1868b790c99bd4ee3f7831e9d3
hanumanthbs <74710737+hanumanthbs@users.noreply.github.com>
2024-08-03 11:42:20 +0530

    changes

--- src/services/ApiWithoutToken.js
+++ src/services/ApiWithoutToken.js
@@ -1,16 +1,16 @@
 import Axios from "axios";
 
 // Local
-// const apiUrl = `http://192.168.0.151:8080`;
+const apiUrl = `http://192.168.0.151:8080`;
 
 // Staging
-const apiUrl = `https://www.stageapi-acharyainstitutes.in/Acharya_University`;
+// const apiUrl = `https://www.stageapi-acharyainstitutes.in/Acharya_University`;
 
 // Kubernetes
 // const apiUrl = `https://api-dev-acharyainstitutes.com`;
 
-const axios = Axios.create({
+const axiosNoToken = Axios.create({
   baseURL: apiUrl,
 });
 
-export default axios;
+export default axiosNoToken;
