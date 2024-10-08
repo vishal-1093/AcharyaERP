3d581edabe39ff1444cd74811bfee140d04850bf
hanumanthbs <74710737+hanumanthbs@users.noreply.github.com>
2024-10-08 16:26:13 +0530

    admission

--- src/services/Constants.js
+++ src/services/Constants.js
@@ -1,7 +1,7 @@
 // local
-const domainUrl = "http://localhost:3001/";
+// const domainUrl = "http://localhost:3001/";
 // Staging
-// const domainUrl = "https://remarkable-bunny-932abb.netlify.app/";
+const domainUrl = "https://remarkable-bunny-932abb.netlify.app/";
 // Kubernetes
 // const domainUrl = "https://kube-pub-acharya-au-frontend-1287324227.ap-south-1.elb.amazonaws.com/";
 export default domainUrl;
