855acfeb2d44443cf43e9873c7fadb364bb4a2d4
Sivaraman <sivaraman2959@acharya.ac.in>
2024-11-26 15:12:54 +0530

    hostel ac_year added and hostel zero free template anount added

--- src/containers/indeces/hostelBedViewIndex/HostelBedViewIndex.jsx
+++ src/containers/indeces/hostelBedViewIndex/HostelBedViewIndex.jsx
@@ -283,10 +283,9 @@ function HostelBedViewIndex({ tab }) {
   const getData = async () => {
     await axios
       .get(
-        `/api/hostel/fetchAllHostelBedAssignment?page=${0}&pageSize=${10000}&sort=createdDate&active=true${
-          tab === "Active Bed"
-            ? "&cancelledStatus=NOT CANCELLED"
-            : "&cancelledStatus=CANCELLED"
+        `/api/hostel/fetchAllHostelBedAssignment?page=${0}&pageSize=${10000}&sort=createdDate&active=true${tab === "Active Bed"
+          ? "&cancelledStatus=NOT CANCELLED"
+          : "&cancelledStatus=CANCELLED"
         }`
       )
       .then((Response) => {
@@ -455,9 +454,11 @@ function HostelBedViewIndex({ tab }) {
               label="Reporting Date"
               value={values.occupiedDate}
               minDate={new Date()}
+              maxDate={new Date(new Date().setMonth(new Date().getMonth() + 10))}
               handleChangeAdvance={handleChangeAdvance}
               required
             />
+
           </Grid>
           <Grid item xs={12} align="right">
             <Button
