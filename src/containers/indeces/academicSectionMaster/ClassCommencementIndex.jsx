6252616ab2600b033751c128d542615c70223a3c
sunny2958 <sunny2958@acharya.ac.in>
2025-01-10 17:57:36 +0530

    Service request transport

--- src/containers/indeces/academicSectionMaster/ClassCommencementIndex.jsx
+++ src/containers/indeces/academicSectionMaster/ClassCommencementIndex.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { Box, Button, IconButton } from "@mui/material";
+import { useState, useEffect, lazy } from "react";
+import { Box, Button, IconButton, Grid } from "@mui/material";
 import GridIndex from "../../../components/GridIndex";
 import { Check, HighlightOff } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
@@ -9,18 +9,36 @@ import CustomModal from "../../../components/CustomModal";
 import axios from "../../../services/Api";
 import { convertToDMY } from "../../../utils/DateTimeUtils";
 import moment from "moment";
+import useBreadcrumbs from "../../../hooks/useBreadcrumbs";
+const CustomAutocomplete = lazy(() =>
+  import("../../../components/Inputs/CustomAutocomplete")
+);
+const initialState = {
+  acYearId: "",
+  schoolId: "",
+  programSpecializationId: "",
+  programAndProgramSpecializationId: "",
+  academicYearList: [],
+  programList: [],
+  programId: "",
+  schoolList: [],
+  programmeSpecializationList: [],
+  loading: false,
+  yearSem: "",
+  yearSemList: []
+};
 
 function ClassCommencementIndex() {
   const [rows, setRows] = useState([]);
+  const [{ acYearId, schoolId, programSpecializationId, programAndProgramSpecializationId, programList, programId, academicYearList, schoolList, programmeSpecializationList, loading, yearSem, yearSemList }, setState] = useState([initialState]);
   const [modalContent, setModalContent] = useState({
     title: "",
     message: "",
     buttons: [],
   });
   const [modalOpen, setModalOpen] = useState(false);
-
   const navigate = useNavigate();
-
+  const setCrumbs = useBreadcrumbs();
   const columns = [
     { field: "commencement_type", headerName: "Commencement", flex: 2.5 },
     { field: "ac_year", headerName: "AC Year", flex: 1 },
@@ -73,11 +91,12 @@ function ClassCommencementIndex() {
       headerName: "Update",
       getActions: (params) => [
         <IconButton
+        disabled={!params.row.active}
           onClick={() =>
             navigate(`/CalendarAcademic/Commencement/Update/${params.row.id}`)
           }
         >
-          <EditIcon />
+          <EditIcon color={params.row.active ? "primary":"secondary"}/>
         </IconButton>,
       ],
     },
@@ -107,13 +126,146 @@ function ClassCommencementIndex() {
     },
   ];
   useEffect(() => {
+    setCrumbs([{}]);
     getData();
+    getAcademicYearData();
+    getSchoolData();
   }, []);
 
-  const getData = async () => {
+  const getAcademicYearData = async () => {
+    await axios
+      .get(`api/academic/academic_year`)
+      .then((res) => {
+        setState((prevState) => ({
+          ...prevState,
+          academicYearList: res?.data?.data.map((el) => ({
+            label: el.ac_year,
+            value: el.ac_year_id,
+          })),
+        }));
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const getSchoolData = async () => {
+    try {
+      const res = await axios.get(`api/institute/school`);
+      if (res?.data?.data?.length) {
+        setState((prevState) => ({
+          ...prevState,
+          schoolList: res?.data?.data.map((el) => ({
+            label: el.school_name_short,
+            value: el.school_id,
+          })),
+        }));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getProgramData = async (schoolId) => {
+    try {
+      if (schoolId) {
+        const res = await axios.get(
+          `/api/academic/fetchAllProgramsWithSpecialization/${schoolId}`
+        );
+        if (res?.data?.data?.length) {
+          setState((prevState) => ({
+            ...prevState,
+            programmeSpecializationList: res?.data?.data.map((el) => ({
+              label: el.specialization_with_program,
+              value: el.program_specialization_id,
+              numberOfSem: el.number_of_semester,
+              numberOfYear: el.number_of_years,
+              programTypeName: el.program_type_name,
+              programId: el.program_assignment_id,
+              programSpecializationId: el.program_specialization_id
+            })),
+          }));
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChangeAdvance = (name, newValue) => {
+    if (name == "schoolId") {
+      handleProgram();
+      getProgramData(newValue);
+    } else if (name == "programAndProgramSpecializationId") {
+      getYearSem(newValue)
+    }
+    setState((prev) => ({
+      ...prev,
+      [name]: newValue,
+    }));
+  };
+
+  const getYearSem = (id) => {
+    const programDetails = programmeSpecializationList.find((ele) => ele.value == id);
+    let { programTypeName, numberOfSem, numberOfYear, programSpecializationId, programId } = programDetails;
+    if (programTypeName == "SEMESTER") {
+      const yearSemLists = Array.from(
+        { length: numberOfSem },
+        (_, i) => ({
+          label: `Sem ${i + 1}`,
+          value: Number(`${i + 1}`)
+        })
+      );
+      setState((prevState) => ({
+        ...prevState,
+        yearSemList: yearSemLists,
+        programId: programId,
+        programSpecializationId: programSpecializationId
+      }));
+    } else {
+      const yearSemLists = Array.from(
+        { length: numberOfYear },
+        (_, i) => ({
+          label: `Year ${i + 1}`,
+          value: Number(`${i + 1}`)
+        })
+      );
+      setState((prevState) => ({
+        ...prevState,
+        yearSemList: yearSemLists,
+        programId: programId,
+        programSpecializationId: programSpecializationId
+      }));
+    }
+  };
+
+  const handleProgram = () => {
+    setState((prevState) => ({
+      ...prevState,
+      programmeSpecializationList: [],
+      programSpecializationId: [],
+    }));
+  };
+
+  const handleFilter = () => {
+    let formValue = {
+      "acYearId": acYearId || "",
+      "schoolId": schoolId || "",
+      "programSpecializationId": programSpecializationId || "",
+      "programId": programId || "",
+      "yearSem": yearSem || ""
+    };
+    getData(formValue)
+  }
+
+  const getData = async (value) => {
+    let params = {};
+    if (value) {
+      params = `page=${0}&page_size=${1000000}&sort=created_date&ac_year_id=${value.acYearId}&school_id=${value.schoolId}&program_assignment_id=${value.programId}&program_specialization_id=${value.programSpecializationId}&year_sem=${value.yearSem}`
+    } else {
+      params = `page=${0}&page_size=${1000000}&sort=created_date`
+    }
     await axios
       .get(
-        `/api/academic/fetchAllClassCommencementDetails?page=${0}&page_size=${10000}&sort=created_date`
+        `/api/academic/fetchAllClassCommencementDetails?${params}`
       )
       .then((res) => {
         setRows(res.data.data.Paginated_data.content);
@@ -147,21 +299,21 @@ function ClassCommencementIndex() {
     };
     params.row.active === true
       ? setModalContent({
-          title: "Deactivate",
-          message: "Do you want to make it Inactive?",
-          buttons: [
-            { name: "Yes", color: "primary", func: handleToggle },
-            { name: "No", color: "primary", func: () => {} },
-          ],
-        })
+        title: "Deactivate",
+        message: "Do you want to make it Inactive?",
+        buttons: [
+          { name: "Yes", color: "primary", func: handleToggle },
+          { name: "No", color: "primary", func: () => { } },
+        ],
+      })
       : setModalContent({
-          title: "",
-          message: "Do you want to make it Active?",
-          buttons: [
-            { name: "Yes", color: "primary", func: handleToggle },
-            { name: "No", color: "primary", func: () => {} },
-          ],
-        });
+        title: "",
+        message: "Do you want to make it Active?",
+        buttons: [
+          { name: "Yes", color: "primary", func: handleToggle },
+          { name: "No", color: "primary", func: () => { } },
+        ],
+      });
     setModalOpen(true);
   };
 
@@ -175,15 +327,68 @@ function ClassCommencementIndex() {
         buttons={modalContent.buttons}
       />
       <Box sx={{ position: "relative", mt: 2 }}>
-        <Button
-          onClick={() => navigate("/CalendarAcademic/commencement/New")}
-          variant="contained"
-          disableElevation
-          sx={{ position: "absolute", right: 0, top: -57, borderRadius: 2 }}
-          startIcon={<AddIcon />}
-        >
-          Create
-        </Button>
+        <Grid container rowSpacing={4} columnSpacing={{ xs: 2, md: 4 }} sx={{ mb: 2, display: "flex", alignItems: "center" }}>
+          <Grid item xs={12} md={2}>
+            <CustomAutocomplete
+              name="acYearId"
+              label="Academic Year"
+              value={acYearId}
+              options={academicYearList || []}
+              handleChangeAdvance={handleChangeAdvance}
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12} md={2}>
+            <CustomAutocomplete
+              name="schoolId"
+              label="School"
+              value={schoolId}
+              options={schoolList || []}
+              handleChangeAdvance={handleChangeAdvance}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <CustomAutocomplete
+              name="programAndProgramSpecializationId"
+              label="Program Specialization"
+              value={programAndProgramSpecializationId}
+              options={programmeSpecializationList || []}
+              handleChangeAdvance={handleChangeAdvance}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <CustomAutocomplete
+              name="yearSem"
+              label="Year/Sem"
+              value={yearSem}
+              options={yearSemList || []}
+              handleChangeAdvance={handleChangeAdvance}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={1}>
+            <Button
+              onClick={handleFilter}
+              variant="contained"
+              disableElevation
+            >
+              Filter
+            </Button>
+          </Grid>
+          <Grid item xs={12} md={2} align="right">
+            <Button
+              onClick={() => navigate("/CalendarAcademic/commencement/New")}
+              variant="contained"
+              disableElevation
+              startIcon={<AddIcon />}
+            >
+              Create
+            </Button>
+          </Grid>
+        </Grid>
         <GridIndex rows={rows} columns={columns} />
       </Box>
     </>
---
+++ src/pages/forms/myRequest/AttendServiceTransportRequest.jsx
@@ -0,0 +1,214 @@
+import { useState, useEffect, lazy } from "react";
+import { Box, Grid, Button, CircularProgress } from "@mui/material";
+import CustomTextField from "../../../components/Inputs/CustomTextField";
+import axios from "../../../services/Api";
+import { useNavigate, useLocation } from "react-router-dom";
+import useAlert from "../../../hooks/useAlert";
+import FormWrapper from "../../../components/FormWrapper";
+import useBreadcrumbs from "../../../hooks/useBreadcrumbs";
+import CustomAutocomplete from "../../../components/Inputs/CustomAutocomplete";
+const ServiceTransportView = lazy(() => import("../../forms/myRequest/ServiceTransportView"));
+
+const initialValues = {
+  requestStatus: "",
+  attendRemarks: "",
+  allottedPersonName: "",
+  allottedPersonNumber: "",
+  active: true,
+};
+
+const requiredFields = ["requestStatus", "attendRemarks", "allottedPersonName", "allottedPersonNumber"];
+
+function AttendServiceRequest() {
+  const [values, setValues] = useState(initialValues);
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+  const rowData = location?.state?.row;
+  const { setAlertMessage, setAlertOpen } = useAlert();
+  const setCrumbs = useBreadcrumbs();
+  const navigate = useNavigate();
+
+  const checks = {
+    allottedPersonName: [values.allottedPersonName !== ""],
+    allottedPersonNumber: [values.allottedPersonNumber !== ""],
+    requestStatus: [values.requestStatus !== ""],
+    attendRemarks: [values.attendRemarks !== ""],
+  };
+
+  const errorMessages = {
+    allottedPersonName: ["This field required"],
+    allottedPersonNumber: ["This field required"],
+    requestStatus: ["This field required"],
+    attendRemarks: ["This field is required"],
+  };
+
+  useEffect(() => {
+    setCrumbs([
+      { name: "Service Render Transport", link: "/ServiceRenderTransport/AttendRequest" },
+      { name: "Status Submission" }
+    ]);
+  }, []);
+
+  const handleChangeAdvance = async (name, newValue) => {
+    setValues((prev) => ({
+      ...prev,
+      [name]: newValue,
+    }));
+  };
+
+  const statusData = [
+    { label: "Trip Cancelled By Transport Dept", value: "Trip Cancelled By Transport Dept" },
+    { label: "Trip Cancelled By End User", value: "Trip Cancelled By End User" },
+    { label: "Trip Completed", value: "Trip Completed" }
+  ];
+
+  const handleChange = (e) => {
+    if (e.target.name === "complaintStatus") {
+      setValues((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value,
+      }));
+    } else {
+      setValues((prev) => ({
+        ...prev,
+        [e.target.name]: e.target.value,
+      }));
+    }
+  };
+
+  const requiredFieldsValid = () => {
+    for (let i = 0; i < requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (Object.keys(checks).includes(field)) {
+        const ch = checks[field];
+        for (let j = 0; j < ch.length; j++) if (!ch[j]) return false;
+      } else if (!values[field]) return false;
+    }
+    return true;
+  };
+
+  const handleCreate = async (e) => {
+    setLoading(true);
+    const temp = {};
+    temp.active = true;
+    temp.request_status = values.requestStatus;
+    temp.attending_remarks = values.attendRemarks;
+    temp.transport_maintenance_id = rowData?.id;
+    temp.alloted_person_name = values.allottedPersonName;
+    temp.alloted_person_number = values.allottedPersonNumber;
+
+    await axios
+      .put(`/api/updateTransportMaintenance/${rowData?.id}`, temp)
+      .then(async (res) => {
+        setLoading(false);
+        if (res.status === 200 || res.status === 201) {
+          setAlertMessage({
+            severity: "success",
+            message: "Service request updated successfully",
+          });
+          navigate("/ServiceRenderTransport/AttendRequest", { replace: true });
+        } else {
+          setAlertMessage({
+            severity: "error",
+            message: res.data ? res.data.message : "Error Occured",
+          });
+        }
+        setAlertOpen(true);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setAlertMessage({
+          severity: "error",
+          message: error.response ? error.response.data.message : "Error",
+        });
+        setAlertOpen(true);
+      });
+  };
+
+  return (
+    <>
+      <ServiceTransportView id={rowData?.id} />
+      <Box component="form" overflow="hidden" p={1}>
+        <FormWrapper>
+          <Grid
+            container
+            rowSpacing={{ xs: 2, md: 4 }}
+            columnSpacing={{ xs: 2, md: 4 }}
+          >
+            <Grid item xs={12} md={3}>
+              <CustomAutocomplete
+                name="requestStatus"
+                label="Request Status"
+                value={values.requestStatus}
+                options={statusData}
+                handleChangeAdvance={handleChangeAdvance}
+                errors={errorMessages.requestStatus}
+                checks={checks.requestStatus}
+                required
+              />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <CustomTextField
+                name="allottedPersonName"
+                label="Driver Name"
+                value={values.allottedPersonName}
+                handleChange={handleChange}
+                fullWidth
+                errors={errorMessages.allottedPersonName}
+                checks={checks.allottedPersonName}
+                required
+              />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <CustomTextField
+                name="allottedPersonNumber"
+                label="Driver Number"
+                value={values.allottedPersonNumber}
+                handleChange={handleChange}
+                fullWidth
+                errors={errorMessages.allottedPersonNumber}
+                checks={checks.allottedPersonNumber}
+                required
+                type="number"
+              />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <CustomTextField
+                name="attendRemarks"
+                label="Remarks"
+                value={values.attendRemarks}
+                handleChange={handleChange}
+                fullWidth
+                errors={errorMessages.attendRemarks}
+                checks={checks.attendRemarks}
+                required
+              />
+            </Grid>
+
+            <Grid item xs={12} align="right">
+              <Button
+                style={{ borderRadius: 7 }}
+                variant="contained"
+                color="primary"
+                disabled={loading || !requiredFieldsValid()}
+                onClick={handleCreate}
+              >
+                {loading ? (
+                  <CircularProgress
+                    size={25}
+                    color="blue"
+                    style={{ margin: "2px 13px" }}
+                  />
+                ) : (
+                  "Submit"
+                )}
+              </Button>
+            </Grid>
+          </Grid>
+        </FormWrapper>
+      </Box>
+    </>
+  );
+}
+
+export default AttendServiceRequest;
