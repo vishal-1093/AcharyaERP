d6c69f24251dd65f17ec5b0deb06bbe697d56dab
Rakshith12345 <rakshithraghu376@gmail.com>
2025-01-22 10:33:51 +0530

    Changes

--- src/pages/forms/studentMaster/HostelFeePdf.jsx
+++ src/pages/forms/studentMaster/HostelFeePdf.jsx
@@ -376,7 +376,7 @@ const HostelFeePdf = () => {
           </Text>
         </View>
         <View style={styles.row2}>
-          <Text style={styles.label}>Craeted Date </Text>
+          <Text style={styles.label}>Created Date </Text>
           <Text style={styles.colon}>:</Text>
           <Text style={styles.value}>
             {"  "} {moment(data?.[0]?.created_date).format("DD-MM-YYYY")}
